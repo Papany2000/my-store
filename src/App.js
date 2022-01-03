95b53786f703f2274e545674bf7c4bb5cabbd7ab
papany2000 <papany2000@rambler.ru>
2022-01-03 17:28:16 +0300

    api connection

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import Header from './Component/Heder/Header.jsx'
 import Products from './Component/Product/Products';
 import Carts from './Component/Carts/Carts'
 import { BrowserRouter as Router , Routes, Route  } from 'react-router-dom';
-
-
+import { getProducts} from './api/products'
+import CreateProductForm from './Component/Ui/forma/createProductForm'
 
 function App() {
-  const [goods] = useState([
-    {id: 1, name: 'Лечо', price: 500,  image: 'https://img.povar.ru/list/6f/8e/c4/b9/ogurchiki_v_tomatnom_soke-306497.JPG'},
-    {id: 2, name: 'Яблоки', price: 400,  image: 'https://kartinkin.net/uploads/posts/2021-07/thumbs/1626217214_28-kartinkin-com-p-marinovannie-ovoshchi-yeda-krasivo-foto-33.jpg'},
-    {id: 3, name: 'Огурцы', price: 300,  image: 'https://img.povar.ru/list/6d/77/d7/43/ostrie_marinovannie_ogurchiki-308112.JPG'},
-    {id: 4, name: 'Яблоки', price: 400,  image: 'https://kartinkin.net/uploads/posts/2021-07/thumbs/1626217214_28-kartinkin-com-p-marinovannie-ovoshchi-yeda-krasivo-foto-33.jpg'},
-   
-  ])
+  const [goods, setGoods] = useState([]);
+  useEffect(() => {
+    getProducts()
+      .then(
+        (result) => {
+          setGoods(result.data);
+        }
+      )
+  }, [])
   const [cartGoods, setCartGoods] = useState([])
   const createTocart = (newItem) =>{
     const isexsist = cartGoods.filter(elem => elem.id === newItem.id)
@@ -47,11 +49,13 @@ function App() {
        <Router>
       <Header/>
         <Routes>
+        <Route path = "/adminPanel" element = {<CreateProductForm />} />   
      <Route path = "/Products" element = {<Products goods = {goods} createTocart = {createTocart}/>} /> 
      <Route path = "/Carts" element = {<Carts cartGoods = {cartGoods} check = {check } removeTocart = {removeTocart} cartClear = {cartClear} style = {style}  visible = {visible} inVisible = {inVisible} sum = {sum}/>} /> 
      </Routes>
      </Router>
     </div>
+   
    
   );
 }
